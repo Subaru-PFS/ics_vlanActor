b6502e283e43af41768356e72f8b5bb9bd907ccc
Hiroshige Yoshida <hiro@naoj.org>
2021-11-10 13:03:11 -1000

    Fix handling of 'None' received es MHS status values

--- python/vlanActor/agcc.py
+++ python/vlanActor/agcc.py
@@ -20,7 +20,7 @@ class Agcc:
             key.timestamp,
             key.isCurrent,
             key.isGenuine,
-            [x.__class__.baseType(x) for x in key.valueList]
+            [x.__class__.baseType(x) if x is not None else None for x in key.valueList]
         ))
 
         if all((key.name == 'agc_fitsfile', key.isCurrent, key.isGenuine)):
@@ -31,7 +31,7 @@ class Agcc:
     def _getValues(self, key):
 
         valueList = self.actor.models['agcc'].keyVarDict[key].valueList
-        return {x.name: x.__class__.baseType(x) for x in valueList} if len(valueList) > 1 else valueList[0].__class__.baseType(valueList[0])
+        return {x.name: x.__class__.baseType(x) if x is not None else None for x in valueList} if len(valueList) > 1 else valueList[0].__class__.baseType(valueList[0]) if valueList[0] is not None else None
 
     @property
     def filepath(self):
